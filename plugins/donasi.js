1b2c0052c1f51647f19a9fb013e1d61a890e1be9
Wabot2902938 <106576480+Wabot2902938@users.noreply.github.com>
2022-06-08 16:53:54 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 ╭═══════════════
 ║╭─── [ DONASI ] ───
 ║│➸ _*Dana*_✅ : 
-║│- 6282361160044
+║│- 081233738677
 ║│
 ║│➸ _*Pulsa*_✅: 
-║│- 6285212023766
+║│- 081233738677
 ║│
 ║│➸ _*OWNER*_
-║│- wa.me/6287892711054
+║│- wa.me/6281233738677
 ║╰────────────
 ╰═══════════════
 `.trim(), 'Donasi jan asal mencet', 'Owner Bot', '.owner')
