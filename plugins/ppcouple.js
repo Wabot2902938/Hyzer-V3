ba7c882259b9e5cd22e94c1b9d0edd12d80c2c89
Wabot2902938 <106576480+Wabot2902938@users.noreply.github.com>
2022-06-08 18:05:11 +0700

    Create ppcouple.js

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -1,7 +1,7 @@
 let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
-  let res = await fetch('https://api.xteam.xyz/randomimage/ppcouple?APIKEY=cristian9407')
+  let res = await fetch('https://api.lolhuman.xyz/api/random/ppcouple?apikey=Fiktod')
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
