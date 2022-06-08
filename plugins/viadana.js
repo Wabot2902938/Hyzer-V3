fc41d40bd5598cbf843c64bb3759f64d9cd7d10f
Wabot2902938 <106576480+Wabot2902938@users.noreply.github.com>
2022-06-08 23:38:13 +0700

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -2,10 +2,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Gopay
-╠➥ *6287892711054*
+╠➥ *081233738677*
 ║- Owner Bot:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+║- wa.me/6281233738677
+╠═〘 ⁨FikriiSky⁩ 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
