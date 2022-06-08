9e635e5918a148c77a234a50a4b772eaf1cfbb4f
Wabot2902938 <106576480+Wabot2902938@users.noreply.github.com>
2022-06-08 18:25:01 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 Sc Gada Banh
-Jgn Lupa Di Subscribe yaa
 `.trim(), '❖ Sagiri-Bot', 'Siap Banh🗿', 'Thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
