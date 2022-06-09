c635b00991b822d3d9efddd84417bfa23bea718d
Wabot2902938 <106576480+Wabot2902938@users.noreply.github.com>
2022-06-09 14:16:30 +0700

    Update meme_indo.js

--- plugins/meme_indo.js
+++ plugins/meme_indo.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=caliph_71')
+  let url = await fetch('https://api.lolhuman.xyz/api/meme/memeindo?apikey=Fiktod')
   let barbar = await url.json()
 conn.sendButtonImg(m.chat, await (await fetch(barbar.result)).buffer(), '*MEME INDO*', watermark, 'NEXT', '.memeindo', m)
 }
